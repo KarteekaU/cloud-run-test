13287a535af1e8101a8e7e9aebf8015f5d568896
Karteeka Uppalapati <karteekau@gmail.com>
2022-05-02 16:04:48 -0600

    ok

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # cloud run
+This is the repo created for cloud-run lab final for Security Analytics MSBX 5500.
+
+# Contents
+.gitignore, Dockerfile, requirements.txt, docker-compose.yml, main.py, make-request.py, phish-model-1649995335.cloudpickle, .dockerignore, .github
+
+### • a docker-compose command to run Flask container
+```bash
+docker-compose up
+#This command builds my own flask app
+```
+
+### • a docker-compose command to execute make-request.py within a running container
+```bash
+docker-compose exec web python make-request.py
+#This command makes requests for the routes
+```
--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 import os
-
+import numpy as np
+import pandas as pd
 from flask import Flask
+import requests
+from flask import json
+from werkzeug.exceptions import HTTPException
+import logging
 
 app = Flask(__name__)
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    logging.exception(e) # <-- added
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
+
+@app.route('/predict', methods=[ 'POST'])
+def predict():
+    """
+    "expects request.get_json to return a string that expects a valid url"
+    """
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        url = request.get_json()
+        length = len(url)
+        prediction = model.predict_proba([[length]])
+        print(prediction, file = sys.stderr)
+        response_object['prediction'] = prediction.tolist()[0][1]
+    return jsonify(response_object)
 
 
 if __name__ == "__main__":
